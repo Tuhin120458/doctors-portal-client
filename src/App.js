edf6cbd7fe329d85015dec055aca8db0491ea17c
Tuhin Islam <tuhin981172@gmail.com>
2022-05-14 09:27:30 +0600

    add

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from './Pages/About/About';
 import Login from './Pages/Login/Login';
 import Appointment from './Pages/Appointment/Appointment';
 import SignUp from './Pages/Login/SignUp';
+import RequireAuth from './Pages/Login/RequireAuth';
 
 
 function App() {
@@ -18,7 +19,11 @@ function App() {
         <Route path="about" element={<About />} />
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<SignUp></SignUp>} />
-        <Route path="appointment" element={<Appointment />} />
+        <Route path="appointment" element={
+          <RequireAuth>
+            <Appointment />
+          </RequireAuth>
+        } />
       </Routes>
     </div>
   );
